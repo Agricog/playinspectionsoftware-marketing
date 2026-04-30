7b3fac3e723d4f584cc7fe010bed7d77ada54a1f
Agricog <mick@mickstevenson.com>
2026-04-30 15:00:17 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://playinspectionsoftware.co.uk',
-  trailingSlash: 'never',
+  trailingSlash: 'always',
   build: {
     format: 'directory',
   },
